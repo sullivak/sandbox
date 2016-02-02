51d8ce91ba151f66f621efd8bcf02af1c212e400
Kenneth Sullivan <Kenneth Sullivan>
2016-02-01 20:46:48 -0800

    adding update to first working draft

--- transcode_party/transcode_dir.py
+++ transcode_party/transcode_dir.py
@@ -1,10 +1,31 @@
 __author__ = 'sullivan'
 
 import os
+import shutil
+import subprocess
+import time
+import datetime
 
-directory = "/home/sullivan/Data/epic_ctc_analysis"
 
-dirs = filter(lambda x: os.path.isdir(os.path.join(directory, x)), os.listdir(directory))
+base_dir = r'E:\Movies'
+togo_dir = r'H:\MoviesOtherFormat'
 
-for dir in dirs:
-    print dir
+dirs = filter(lambda x: os.path.isdir(os.path.join(base_dir, x)), os.listdir(base_dir))
+
+cnt = 0
+for dir in dirs:	
+	whatsin = os.listdir(os.path.join(base_dir, dir))
+	
+	if 'VIDEO_TS.IFO' in whatsin or 'video_ts.ifo' in whatsin:		
+		print 'shutil.move(r"' + os.path.join(base_dir, dir) + '", r"' + os.path.join(togo_dir, dir) + '")' 
+		shutil.move(os.path.join(base_dir, dir), os.path.join(togo_dir, dir))
+		# print 'move "' + os.path.join(base_dir, dir) + '" "' + os.path.join(togo_dir, dir) + '"'
+		args = ['HandBrakeCLI', '-i', os.path.join(togo_dir, dir),
+				'-o', os.path.join(base_dir, dir) + '.mp4',
+				'-e', 'x264', '-q', '17', '--subtitle', 'scan,1,2,3,4,5,6,7,8,9,10', '-a', '1,2,3,4,5,6,7,8,9,10']
+		#cmd_line = 'HandBrakeCLI -i "' + os.path.join(togo_dir, dir) + '" -o "' + os.path.join(base_dir, dir) + '.mp4"' + ' -e x264 -q 17 --subtitle scan,1,2,3,4,5,6,7,8,9,10 -a 1,2,3,4,5,6,7,8,9,10'
+		print args
+		time_string = st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+		print time_string
+		subprocess.call(args)		
+
